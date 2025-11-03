aef113c210456e4b714cc0b5835b85d161939bd8
Nikola Marinic <nmarinic@evertz.com>
2025-11-03 10:02:29 -0500

    Add colourschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,4 +11,10 @@ return {
   },
   -- add e-ink
   { "e-ink-colorscheme/e-ink.nvim" },
+  -- add seoul256
+  { "junegunn/seoul256.vim" },
+  -- add gruvbox-material
+  { "sainnhe/gruvbox-material" },
+  -- add thorn
+  { "jpwol/thorn.nvim" },
 }
